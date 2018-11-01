afbdb233de8675c45351eb6eca078e2df7708918
Lau Cheng En <chengenlau4912@gmail.com>
2018-11-01 10:22:05 +0800

    Say Hello to the Login Branch!

--- Campfire/LogIn.aspx.designer.cs
+++ Campfire/LogIn.aspx.designer.cs
@@ -11,5 +11,23 @@ namespace Campfire {
     
     
     public partial class LogIn {
+        
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
     }
 }
