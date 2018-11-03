05c7fbb916038906f2ae9a47cbeb68743cc26add
Zach <s10179991@connect.np.edu.sg>
2018-11-03 16:56:28 +0800

    UserRegisterPage
    Added UserRegisterPage Functionality,
    with placeholder webconfig connectionstring. Also added classes folder with new class SQLFunctions.cs which would be used to write all SQL related functions for ease of use.

---
+++ Campfire/Classes/SQLFunctions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Campfire.Classes
+{
+    public class SQLFunctions : System.Web.UI.Page
+    {
+        public void AddRegisterData(string _name, string _username, string _email,
+            string _password)
+        {
+            string strConn = ConfigurationManager.ConnectionStrings
+                    ["CampfireConnectionString"].ToString();
+
+            SqlConnection conn = new SqlConnection(strConn);
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO CampfireUsers " +
+                "(Name, Username, Email, Password) VALUES (@name, @username," +
+                "@email, @password)", conn);
+
+            cmd.Parameters.AddWithValue("@name", _name);
+            cmd.Parameters.AddWithValue("@username", _username);
+            cmd.Parameters.AddWithValue("@email", _email);
+            cmd.Parameters.AddWithValue("@password", _password);
+
+            conn.Open();
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+        }
+    }
+}
---
+++ Campfire/UserRegister/Register.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Campfire.UserRegister
+{
+    public partial class Register : Classes.SQLFunctions
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_submit_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(rfv_name.IsValid) + Convert.ToInt32(rfv_username.IsValid) + Convert.ToInt32(rfv_email.IsValid)
+                + Convert.ToInt32(rfv_password.IsValid) + Convert.ToInt32(rfv_password2.IsValid) + Convert.ToInt32(cv_password2.IsValid) == 6)
+            {
+                string _name = txt_Name.Value;
+                string _username = tb_username.Text.ToString();
+                string _email = email_Email.Value;
+                string _password = tb_password2.Text.ToString();
+
+                AddRegisterData(_name, _username, _email, _password);
+            }
+        }
+    }
+}
