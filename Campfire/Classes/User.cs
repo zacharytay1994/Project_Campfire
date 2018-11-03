0734d4008395cbcf483a0ed3483f558b44a610ec
Zach <s10179991@connect.np.edu.sg>
2018-11-03 17:52:07 +0800

    Update Campfire/Classes/User.cs

---
+++ Campfire/Classes/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Campfire.Classes
+{
+    public class User
+    {
+        private string name { get; set; }
+        private string username { get; set; }
+        private string email { get; set; }
+        private string password { get; set; }
+    }
+}
