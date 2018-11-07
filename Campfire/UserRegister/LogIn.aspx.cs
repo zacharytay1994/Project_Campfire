6b2b28ed99907f70e6bc9899d53cc8901f5fee64
Lau Cheng En <chengenlau4912@gmail.com>
2018-11-07 11:33:05 +0800

    Patch 2.55 (Fixed Login Page)

--- Campfire/UserRegister/LogIn.aspx.cs
+++ Campfire/UserRegister/LogIn.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace Campfire
+namespace Campfire.UserRegister
 {
     public partial class Login : Classes.SQLFunctions
     {
