266a4907da9235b168ff4b7b36e2214e8b27d6b6
chle <Cho@CHO>
2018-11-08 11:26:44 +0800

    updated script

--- eventplanner.sql
+++ eventplanner.sql
@@ -1,18 +1,24 @@
 USE master
-IF EXISTS(select * from sys.databases where name='EventPlanner')
-DROP DATABASE EventPlanner
+IF EXISTS(select * from sys.databases where name='Campfire_test')
+DROP DATABASE Campfire_test
 GO
 
-Create Database EventPlanner
+Create Database Campfire_test
 GO
 
-use EventPlanner
+use Campfire_test
 GO
 
 /* Table: dbo.Account */
 if exists (select * from sysobjects 
-  where id = object_id('dbo.Account') and sysstat & 0xf = 3)
-  drop table dbo.Account
+  where id = object_id('dbo.CampfireUsers') and sysstat & 0xf = 3)
+  drop table dbo.CampfireUsers
+GO
+
+/* Table: dbo.Category */
+if exists (select * from sysobjects 
+  where id = object_id('dbo.Category') and sysstat & 0xf = 3)
+  drop table dbo.Category
 GO
 
 /* Table: dbo.Activities */
@@ -23,24 +29,44 @@ GO
 
 /*** Create tables ***/
 /* Table: dbo.Account 	*/
-CREATE TABLE dbo.Account 
+CREATE TABLE dbo.CampfireUsers 
 (
-  AccountID		int			 NOT NULL,
-  Name			varchar(100) NOT NULL,
-  EmailAddr		varchar(150) NOT NULL,
-  Password		varchar(255) NOT NULL,
+  AccountID		int				NOT NULL,
+  Username		varchar(100)	NOT NULL,
+  EmailAddr		varchar(150)	NOT NULL,
+  Password		varchar(255)	NOT NULL,
+  CONSTRAINT PK_Users PRIMARY KEY NONCLUSTERED (AccountID)
 )
 GO
 
-/*** Create tables ***/
+/* Table: dbo.Category */
+CREATE TABLE dbo.Category
+(
+  CategoryID		int			 NOT NULL,
+  CategoryName		varchar(255) NOT NULL,
+  CONSTRAINT PK_Category PRIMARY KEY NONCLUSTERED (CategoryID)
+)
+
 /* Table: dbo.Activities */
 CREATE TABLE dbo.Activities
 (
   ActivityID	int			 NOT NULL,
   ActivityName	varchar(255) NOT NULL,
   ActivityDesc	varchar(255) NOT NULL,
-  Category		varchar(255) NOT NULL,
-  ActivityExp	char(255) NOT NULL,
-  Link			varchar(255)
+  CategoryID	int			 NOT NULL,
+  ActivityExp	char(255)	 NOT NULL,
+  Link			varchar(255)	 NULL,
+  Votes			int				 NULL
+  CONSTRAINT PK_Activities PRIMARY KEY NONCLUSTERED (ActivityID),
+  CONSTRAINT FK_Activities_Category FOREIGN KEY (CategoryID) REFERENCES dbo.Category(CategoryID)
 )
 GO
+
+/* Add foreign key constraint to dbo.Category */
+ALTER TABLE dbo.Category
+  ADD CONSTRAINT FK_Category_CategoryID FOREIGN KEY (CategoryID) REFERENCES      
+  dbo.Activities(ActivityID)
+GO
+
+INSERT INTO CampfireUsers VALUES (1, 'Zach', 'zacharytay1994@gmail.com', '12345678')
+INSERT INTO CampfireUsers VALUES (2, 'cho', 'surellity@gmail.com', '12345678')
